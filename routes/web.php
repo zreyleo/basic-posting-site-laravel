5bb1e69e269a3dbcc9f9333b3bf02fae9acef441
Regynald Zambrano <zreyleo@gmail.com>
2021-07-02 17:37:52 -0500

    posts index page finished

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('post', 'PostController');
+Route::resource('posts', 'Backend\PostController')
+    ->middleware('auth')
+    ->except('show');
